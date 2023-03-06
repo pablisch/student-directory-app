9fcca2613c5d83dab5eec2e58ed878352ae68a73
Pablo <pablito@me.com>
2023-03-06 13:46:01 +0000

    Changed student nested array into an array of hashes.
    Added keys for name and cohort to make the print_list method more decalrative.

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # create an array of students
 students = [
-["Dr. Hannibal Lecter", :november ],
-["Darth Vader", :november ],
-["Nurse Ratchet", :november ],
-["Michael Corleone", :november ],
-["Alex DeLarge", :november ],
-["The Wicked Witch of the West", :november ],
-["Terminator", :november ],
-["Freddy Krueger", :november ],
-["The Joker", :november ],
-["Joffrey Baratheon", :november ],
-["Norman Bates", :november ],
+{name: "Dr. Hannibal Lecter", cohort: :november },
+{name: "Darth Vader", cohort: :november },
+{name: "Nurse Ratchet", cohort: :november },
+{name: "Michael Corleone", cohort: :november },
+{name: "Alex DeLarge", cohort: :november },
+{name: "The Wicked Witch of the West", cohort: :november },
+{name: "Terminator", cohort: :november },
+{name: "Freddy Krueger", cohort: :november },
+{name: "The Joker", cohort: :november },
+{name: "Joffrey Baratheon", cohort: :november },
+{name: "Norman Bates", cohort: :november }
 ]
 
 # method to print student list header
@@ -20,13 +20,13 @@ def print_header
 end
 
 # method to print the student list names list body
-def print_list(names)
-  names.each { |name| puts "#{name[0]} (#{name[1]} cohort)" }
+def print_list(students)
+  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 # method to print the student list footer
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students."
 end
 
 # call methods to print out all student list components
