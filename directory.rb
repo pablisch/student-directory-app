8014a15dd4a7263d6eb91e6bd65288dfcb115f70
Pablo <pablito@me.com>
2023-03-08 16:24:24 +0000

    added interactive menu to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,7 +1,30 @@
 # Solution to Makers' precourse Week 4 Step 8 Task 11
 # https://makersacademy.teachable.com/courses/256825/lectures/3989219
 
-
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
 
 def input_students
   puts "Please enter the names of the students"
@@ -38,10 +61,11 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great student#{"s" if names.size > 1}"
 end
 
+interactive_menu
 
-students = input_students
-if students.size > 0
-  print_header
-  print(students)
-  print_footer(students)
-end
+# students = input_students
+# if students.size > 0
+#   print_header
+#   print(students)
+#   print_footer(students)
+# end
