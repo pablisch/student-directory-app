ce74670ecb8b4c9ce1423c61a1d57c248d160270
Pablo <pablito@me.com>
2023-03-08 19:05:55 +0000

    Added file argument code to directory.rb

--- directory.rb
+++ directory.rb
@@ -5,9 +5,10 @@
 
 def interactive_menu
   load_students
+  show_students
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -46,11 +47,11 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} student#{"s" if @students.size > 1}"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -63,8 +64,20 @@ def save_students
   end
 end
 
-def load_students
-  file = File.open("students.csv")
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+def load_students(filename = "students.csv")
+  file = File.open("filename")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << { name: name, cohort: cohort.to_sym }
