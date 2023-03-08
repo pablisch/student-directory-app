9d70e985c8d2ae83616728be63bd55e03651b6a4
Pablo <pablito@me.com>
2023-03-08 16:58:27 +0000

    Refactored after adding interactive menu

--- directory.rb
+++ directory.rb
@@ -1,71 +1,64 @@
 # Solution to Makers' precourse Week 4 Step 8 Task 11
 # https://makersacademy.teachable.com/courses/256825/lectures/3989219
 
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_menu
+  puts
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
-
-  name = gets.tr("\n", "")
-  # while the name is not empty, repeat this code
+  name = gets.chomp
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} student#{"s" if students.size > 1}"
-    # get another name from the user
-    name = gets.tr("\n", "")
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} student#{"s" if @students.size > 1}"
+    name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 def print_header
-  puts "The students of my cohort at Makers Academy"
+  puts "The students of Makers Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+def print_student_list
+  @students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great student#{"s" if names.size > 1}"
+def print_footer
+  puts "Overall, we have #{@students.count} great student#{"s" unless @students.size == 1 }"
 end
 
 interactive_menu
 
-# students = input_students
-# if students.size > 0
-#   print_header
-#   print(students)
-#   print_footer(students)
-# end
