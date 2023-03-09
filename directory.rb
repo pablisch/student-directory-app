6dd9527813f7955495dc5c2c2f61fa63f5b5fa2f
Pablo <pablito@me.com>
2023-03-09 22:26:56 +0000

    completed step14.7

--- directory.rb
+++ directory.rb
@@ -53,17 +53,21 @@ def choose_filename
 end
 
 def load_students
+  @students = []
   filename = choose_filename
   unless File.exists?(filename)
     puts "No such file"
     exit
   end
-  File.open(filename) do |file|
-    file.readlines.each do |line|
-      name, cohort = line.chomp.split(",")
-      create_students_array(name)
-    end
+  CSV.foreach(filename) do |row|
+    @students << {name: row[0], cohort: row[1]}
   end
+  # File.open(filename, "r") do |file|
+  #   file.readlines.each do |line|
+  #     name, cohort = line.chomp.split(",")
+  #     create_students_array(name)
+  #   end
+  # end
   puts "#{@students.size} students have been loaded from #{filename}"
   puts
 end
@@ -93,12 +97,15 @@ end
 
 def save_students
   filename = choose_filename
-  file = File.open(filename, "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  CSV.open(filename, "w") do |csv| 
+    @students.each { |student| csv << [student[:name], student[:cohort]] } 
   end
+  # file = File.open(filename, "w")
+  # @students.each do |student|
+  #   student_data = [student[:name], student[:cohort]]
+  #   csv_line = student_data.join(",")
+  #   file.puts csv_line
+  # end
   puts "#{@students.size} students have been saved to #{filename}"
 end
 
